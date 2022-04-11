cb943fde490395debcab87aae03327c0c4f14066
KMininkov <mininkov3228@gmail.com>
2022-04-11 20:05:42 +0300

    added lab-2

---
+++ lab_2/WindowsForm/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelM = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonOpenPort = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.panelM.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SeaGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(19, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Алгоритм №1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panelM
+            // 
+            this.panelM.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelM.Controls.Add(this.label1);
+            this.panelM.Controls.Add(this.comboBox1);
+            this.panelM.Controls.Add(this.buttonOpenPort);
+            this.panelM.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelM.Location = new System.Drawing.Point(0, 0);
+            this.panelM.Name = "panelM";
+            this.panelM.Size = new System.Drawing.Size(340, 39);
+            this.panelM.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(17, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "COM порт";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.Location = new System.Drawing.Point(122, 6);
+            this.comboBox1.MaxDropDownItems = 10;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(88, 26);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
+            // 
+            // buttonOpenPort
+            // 
+            this.buttonOpenPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOpenPort.Location = new System.Drawing.Point(239, 6);
+            this.buttonOpenPort.Name = "buttonOpenPort";
+            this.buttonOpenPort.Size = new System.Drawing.Size(88, 25);
+            this.buttonOpenPort.TabIndex = 5;
+            this.buttonOpenPort.Tag = "1";
+            this.buttonOpenPort.Text = "Відкрити";
+            this.buttonOpenPort.UseVisualStyleBackColor = true;
+            this.buttonOpenPort.Click += new System.EventHandler(this.buttonOpenPort_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SeaGreen;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(211, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 49);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Алгоритм №2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.SkyBlue;
+            this.panel3.Location = new System.Drawing.Point(101, 185);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(30, 30);
+            this.panel3.TabIndex = 28;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.SkyBlue;
+            this.panel2.Location = new System.Drawing.Point(65, 185);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(30, 30);
+            this.panel2.TabIndex = 27;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SkyBlue;
+            this.panel1.Location = new System.Drawing.Point(29, 185);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(30, 30);
+            this.panel1.TabIndex = 26;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 850;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.SkyBlue;
+            this.panel4.Location = new System.Drawing.Point(137, 185);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(30, 30);
+            this.panel4.TabIndex = 29;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.SkyBlue;
+            this.panel5.Location = new System.Drawing.Point(173, 185);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(30, 30);
+            this.panel5.TabIndex = 30;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.SkyBlue;
+            this.panel6.Location = new System.Drawing.Point(208, 185);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(30, 30);
+            this.panel6.TabIndex = 30;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.SkyBlue;
+            this.panel7.Location = new System.Drawing.Point(244, 185);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(30, 30);
+            this.panel7.TabIndex = 30;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.SkyBlue;
+            this.panel8.Location = new System.Drawing.Point(280, 185);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(30, 30);
+            this.panel8.TabIndex = 30;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 850;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 294);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panelM);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panelM.ResumeLayout(false);
+            this.panelM.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonOpenPort;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
